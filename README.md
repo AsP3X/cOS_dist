ae1f83ecd25f4bf195fdec1c6e1f3677b3409afb
AsP3X <services@vorberg-webmail.de>
2022-11-22 21:06:09 +0100

    TASK: added error capture to create script

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # cOS_dist
 
-export DIST_ROOT=/home/asp3x/Desktop/Development/00_corespace/cOS_dist/
+export DIST_ROOT=/home/asp3x/Desktop/Development/00_corespace/cOS_dist
 export LFS=$DIST_ROOT/build_env/build_root
 
 // add user to sudoers
--- build_env/create.sh
+++ build_env/create.sh
@@ -1,3 +1,4 @@
+set -e
 echo "Dist Root: ${DIST_ROOT:?}"
 echo "LFS: ${LFS:?}"
 
@@ -7,3 +8,7 @@ if ! test $(whoami) == "cbuild" ; then
 fi
 
 echo "Creating build environment..."
+cd $DIST_ROOT/build_env
+
+bash -e build_scripts/binutils-pass-1.sh
+bash -e build_scripts/gcc-pass-1.sh
