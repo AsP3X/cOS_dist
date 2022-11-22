cb50594c8a1047f5acc23a8e03a03edb9039ecde
AsP3X <services@vorberg-webmail.de>
2022-11-22 23:59:31 +0100

    TASK: added diffutils installer

---
+++ build_env/build_scripts/diffutils.sh
@@ -0,0 +1,9 @@
+BUILD_ARCHIVE=diffutils-3.8.tar.xz
+. $DIST_ROOT/build_env/build_scripts/inc-start.sh $BUILD_ARCHIVE
+
+./configure --prefix=/usr --host=$LFS_TGT
+
+make
+make DESTDIR=$LFS install
+
+. $DIST_ROOT/build_env/build_scripts/inc-end.sh $BUILD_ARCHIVE
